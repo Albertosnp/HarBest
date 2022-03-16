cd86a781f55149b49b4b8a8347b5b030a633722b
Alberto Snp <albertosnp91@gmail.com>
2022-03-16 19:49:28 +0100

    feat: Add pagination

--- src/services/getProducts.service.js
+++ src/services/getProducts.service.js
@@ -2,21 +2,20 @@ import axios from "axios";
 
 const API_URL = "http://localhost:9000/";
 
-export const getProducts = async () => {
-  const response = await axios.create(
-    {
+export const getProducts = async ({ page }) => {
+  const response = await axios
+    .create({
       baseURL: API_URL,
       headers: {
         "Content-Type": "application/json",
       },
       params: {
-        itemsPerPage: 5,
-        page: 1,
+        itemsPerPage: 4,
+        page,
         active: true,
       },
-    }
-  ).get("/products");
-  
-  
+    })
+    .get("/products");
+
   return response.data;
 };
--- src/views/Home.js
+++ src/views/Home.js
@@ -6,13 +6,14 @@ import { getProducts } from "../services/getProducts.service";
 export const Home = () => {
   const [products, setProducts] = useState(null);
   const [totalCount, setTotalCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     let isSubscribed = true;
 
     const fetchData = async () => {
       try {
-        const { list, totalCount } = await getProducts();
+        const { list, totalCount } = await getProducts({ page: currentPage });
         if (isSubscribed) {
           setProducts(list);
           setTotalCount(totalCount);
@@ -25,7 +26,13 @@ export const Home = () => {
     return () => {
       isSubscribed = false;
     };
-  }, []);
+  }, [currentPage]);
+
+  const handlePageChange = (page) => {
+    if (currentPage + page <= 0) return;
+    if (currentPage + page >= 5) return;
+    setCurrentPage(prev => prev + page);
+  };
 
   return (
     <div>
@@ -35,15 +42,16 @@ export const Home = () => {
           products?.map(({ _id, description, name, price }) => (
             <li key={_id}>
               <Link to={`/${_id}`}>
-                <Product 
-                  description={description}
-                  name={name}
-                  price={price}
-                />
+                <Product description={description} name={name} price={price} />
               </Link>
             </li>
           ))}
       </ul>
+      <section>
+        <button onClick={() => handlePageChange(-1)}>Prev</button>
+        {currentPage}
+        <button onClick={() => handlePageChange(1)}>Next</button>
+      </section>
     </div>
   );
 };
