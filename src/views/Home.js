ad1a52c0f434e75753800521178ccf40fe5fd2e4
Alberto Snp <albertosnp91@gmail.com>
2022-03-17 18:07:55 +0100

    feat: Add redux store connection

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,55 +1,74 @@
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { ListItem } from "../components/ListItem/ListItem";
 import { Product } from "../components/Product/Product";
-import { getProducts } from "../services/getProducts.service";
+import { getProductsAction } from "../store/reducers/productsStore";
 
 export const Home = () => {
-  const [products, setProducts] = useState(null);
-  const [totalCount, setTotalCount] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.productsStore.products);
+  const [loading, setLoading] = useState(true);
+  const [actives, setActives] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
-    let isSubscribed = true;
+    if (!loading) setLoading(true);
 
-    const fetchData = async () => {
-      try {
-        const { list, totalCount } = await getProducts({ page: currentPage });
-        if (isSubscribed) {
-          setProducts(list);
-          setTotalCount(totalCount);
-        }
-      } catch (error) {
-        console.log({ error });
-      }
-    };
-    fetchData();
+    dispatch(getProductsAction({ page: currentPage, active: actives }));
+    setLoading(false);
+
     return () => {
-      isSubscribed = false;
+      setLoading(false);
     };
-  }, [currentPage]);
+  }, [currentPage, dispatch, actives]);
+
+  useEffect(() => {
+    dispatch(getProductsAction({}));
+  }, [dispatch]);
 
   const handlePageChange = (page) => {
-    if (currentPage + page <= 0) return;
-    if (currentPage + page >= 5) return;
-    setCurrentPage(prev => prev + page);
+    if (currentPage + page <= -1) return;
+    if (currentPage + page >= 4) return;
+    setCurrentPage((prev) => prev + page);
+  };
+
+  const handleFilterActive = () => {
+    setActives(!actives);
   };
 
   return (
     <div>
       <h1>Productos</h1>
-      <ul>
-        {products &&
-          products?.map(({ _id, description, name, price }) => (
-            <li key={_id}>
-              <Link to={`/${_id}`}>
-                <Product description={description} name={name} price={price} />
-              </Link>
-            </li>
-          ))}
-      </ul>
+      {loading ? (
+        <div>Cargando...</div>
+      ) : (
+        <div>
+          { actives ? (
+            <button onClick={handleFilterActive}>Activos</button>
+          ) : (
+            <button onClick={handleFilterActive}>Inactivos</button>
+          )}
+          <ul>
+            {products &&
+              products?.map((product) => (
+                <ListItem
+                  key={product._id}
+                  children={
+                    <Link
+                      key={product._id}
+                      to={`/${product._id}`}
+                      children={<Product {...product} />}
+                    />
+                  }
+                />
+              ))}
+          </ul>
+        </div>
+      )}
       <section>
         <button onClick={() => handlePageChange(-1)}>Prev</button>
-        {currentPage}
+        {currentPage + 1}
         <button onClick={() => handlePageChange(1)}>Next</button>
       </section>
     </div>
